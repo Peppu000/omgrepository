a56621bd12540c48a8d1d3447d1fcb5e62c95c14
Peppu000 <100580457+Peppu000@users.noreply.github.com>
2025-07-26 13:50:10 +0900

    Fix deploy.sh (2)

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 target_branch="ghp_deploy"
 git config --global user.name "CircleCI deployer"
 git config --global user.email "<>"
-git checkout $target_branch
+git checkout -b $target_branch
 git reset --hard origin/main
 
 ./target/release/omgrepository > output.txt
