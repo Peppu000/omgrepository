e136fc181334e92313f7d12fe0966df7da04bfbd
Peppu000 <100580457+Peppu000@users.noreply.github.com>
2025-07-26 13:08:33 +0900

    Enable automatic deployment

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -ex
+target_branch="ghp_deploy"
+git config --global user.name "CircleCI deployer"
+git config --global user.email "<>"
+git checkout $target_branch
+git reset --hard origin/main
+
+./target/release/omgrepository > output.txt
+git add output.txt
+git commit -m "Update Github Pages"
+git push origin $target_branch
